4f7f9ee9b771afd5dbe8067f434eace688de0f15
Lerenard <lerenard.charly@gmail.com>
2021-01-07 13:56:33 +0100

    css: move to scss

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React, {useState } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthContext } from "./context/auth";
--- code/src/pages/createGame/CreateGame.js
+++ code/src/pages/createGame/CreateGame.js
@@ -1,49 +1,111 @@
 import React, { Component } from "react";
 import { Card, Form, Input, Button } from "../../components/AuthForm";
+import { DropdownList } from 'react-widgets'
+import './CreateGame.css';
 import PropTypes from 'prop-types'
+
 
 class CreateGame extends Component{
 
     constructor(props){
         super(props);
-        this.state = {gameName: "HELLO"}
+
+        // let { DropdownList }
+        let colors = ['orange', 'red', 'blue', 'purple']   
+
+        this.state = {
+            gameName: "", 
+            colors: colors,
+            listOpen: false,
+            headerTitle: this.props.title}
 
         this.handleChange = this.handleChange.bind(this);
+
     }
 
+    // init form
     handleChange(event) {
         this.setState({gameName: event.target.value});
     }
 
+    handleClickOutside(){
+        this.setState({
+          listOpen: false
+        })
+      }
+      toggleList(){
+        this.setState(prevState => ({
+          listOpen: !prevState.listOpen
+        }))
+      }
+
 
     render(){
+        const{list} = this.props
+        const{listOpen, headerTitle} = this.state
         return(
-            <Card>
-                {/*<Logo src={logoImg} />*/}
+            <Card className="backgroundApp">
                 
-                <h1>Create game</h1>
+                {/********************** TITLE  **********************/}
+                <h1 className="title">Create a game</h1>
                 
                 <Form>
-                    <Input
-                        type="text"
-                        value={this.state.gameName}
-                        placeholder="game name*"
-                        onChange={this.handleChange}
-                    />
+                  {/********************** GAME NAME  **********************/}
+                  <Input
+                      className="input"
+                      type="text"
+                      value={this.state.gameName}
+                      placeholder="game name*"
+                      onChange={this.handleChange}
+                  />
+                  <br></br>
 
-                    <Button>Create saloon</Button>
-                    {/* <Button onClick={createSaloon}>Create saloon</Button> */}
+                  {/********************** PLAYER NUMBER SELECTOR  **********************/}
+                    {/* <>
+                      <DropdownList
+                    data={this.state.colors}
+                    defaultValue={"blue"}
+                    disabled={["red", "purple"]}/>
+                    </> */}
+
+                  <select className="sc-dlfnbm boijVN input" id="playerNumberSelector">
+                    <option className="playerNumberOption" value="2">2</option>
+                    <option className="playerNumberOption" value="3">3</option>
+                    <option className="playerNumberOption" value="4">4</option>
+                    <option className="playerNumberOption" value="5">5</option>
+                  </select>
+                  <br></br>
+
+                  {/********************** PROPERTY SELECTOR  **********************/}
+                  <select className="sc-dlfnbm boijVN input" id="propertySelector">
+                    <option className="propertyOption" value="Private">Private</option>
+                    <option className="propertyOption" value="Public">Public</option>
+                  </select>
+                  <br></br>
+
+                  {/********************** LEVEL SELECTOR  **********************/}
+                  <select className="sc-dlfnbm boijVN input" id="levelSelector">
+                    <option className="levelOption" value="Level 1">Level 1</option>
+                    <option className="levelOption" value="Level 2">Level 2</option>
+                    <option className="levelOption" value="Level 3">Level 3</option>
+                  </select>
+
+                  <br></br>
+
+                  {/********************** CREATE GAME BUTTON  **********************/}
+                  <Button className="buttonA">Create saloon</Button>
+
                 </Form>
             </Card> 
-        )
+
+        )      
     }
-        
 }
 
 // createSaloon = ({}) => (return true)
 
-CreateGame.propTypes = {
-    gameName: PropTypes.string.isRequired
-}
+// CreateGame.propTypes = {
+//     state.gameName: PropTypes.string.isRequired
+// }
 
 export default CreateGame;
