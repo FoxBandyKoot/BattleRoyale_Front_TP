e9cf93b2e1e632d39e3c61265cf78913ed808931
Lerenard <lerenard.charly@gmail.com>
2021-01-10 11:24:45 +0100

    refacto(css): add 7-1 architecture pattern css (folder scss), replace custom tag by new classes

--- code/src/components/AuthForm.js
+++ code/src/components/AuthForm.js
@@ -1,47 +1,48 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Card = styled.div`
-  box-sizing: border-box;
-  max-width: 410px;
-  margin: 0 auto;
-  padding: 0 2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// const Card = styled.div`
+//   box-sizing: border-box;
+//   max-width: 410px;
+//   margin: 0 auto;
+//   padding: 0 2rem;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
+// const Form = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+// `;
 
-const Input = styled.input`
-  padding: 1rem;
-  border: 1px solid #999;
-  margin-bottom: 1rem;
-  font-size: 0.8rem;
-`;
+// const Input = styled.input`
+//   padding: 1rem;
+//   margin: 10px;
+//   border: 1px solid #999;
+//   margin-bottom: 1rem;
+//   font-size: 0.8rem;
+//   border-radius: 35px
+// `;
 
-const Button = styled.button`
-  background: linear-gradient(to bottom, #6371c7, #5563c1);
-  border-color: #3f4eae;
-  border-radius: 3px;
-  padding: 1rem;
-  color: white;
-  font-weight: 700;
-  width: 100%;
-  margin-bottom: 1rem;
-  font-size: 0.8rem;
-`;
+// const Button = styled.button`
+//   background-color: #F05029;
+//   border-radius: 35px;
+//   padding: 1rem;
+//   color: white;
+//   font-weight: 700;
+//   width: 100%;  
+//   margin-bottom: 1rem;
+//   font-size: 0.8rem;
+// `;
 
-const Logo = styled.img`
-  width: 50%;
-  margin-bottom: 1rem;
-`;
+// const Logo = styled.img`
+//   width: 50%;
+//   margin-bottom: 1rem;
+// `;
 
-const Error = styled.div`
-  background-color: red;
-`;
+// const Error = styled.div`
+//   background-color: red;
+// `;
 
-export { Form, Input, Button, Logo, Card, Error };
+// export { Form, Logo, Card, Error };
