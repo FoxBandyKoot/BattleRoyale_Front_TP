b8ee7020e52cb3e4dc02d3de189a59138ac9c359
Alexis Brohan <alexis.brohan@imie.fr>
2021-01-28 18:26:50 +0100

    Push notif

--- code/public/sw.js
+++ code/public/sw.js
@@ -70,21 +70,14 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('push', function(e) {
     var options = {
-        body: 'This notification was generated from a push!',
-        icon: 'images/example.png',
+        body: 'It\'s your turn !',
         vibrate: [100, 50, 100],
         data: {
             dateOfArrival: Date.now(),
             primaryKey: '2'
         },
-        actions: [
-            {action: 'explore', title: 'Explore this new world',
-                icon: 'images/checkmark.png'},
-            {action: 'close', title: 'Close',
-                icon: 'images/xmark.png'},
-        ]
     };
     e.waitUntil(
-        self.registration.showNotification('Hello world!', options)
+        self.registration.showNotification('Battle-royal', options)
     );
 });
