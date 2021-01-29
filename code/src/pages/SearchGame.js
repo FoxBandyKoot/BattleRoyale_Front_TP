e8c3509d2234b2163411147b64922ea03fc8cfdb
FoxBandyKoot <32387837+FoxBandyKoot@users.noreply.github.com>
2021-01-29 09:13:45 +0100

    Cle/fix create game, signup, and refacto
    * Merge branch 'origin/develop'
    
    * Merge branch 'origin/develop'
    
    * fix: fix and refacto signup
    
    * feat, refacto: createGame, some buttons and properties commented

--- code/src/pages/SearchGame.js
+++ code/src/pages/SearchGame.js
@@ -1,9 +1,7 @@
-import React, { useRef, Component } from "react";
-import { observer } from "mobx-react-lite";
+import React, { Component } from "react";
 import Menu from "../components/Menu";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
-import SearchGameStore from "../observers/SearchGameStore";
 
 class SearchGame extends Component {
 
@@ -15,15 +13,11 @@ class SearchGame extends Component {
         this.state = {
             nameCreatedGame: [],
             mapCreatedGame: [],
-            properties: ['Publique', 'Privée'],
+            // properties: ['Publique', 'Privée'],
             maps: ['Toutes', 'Verte', 'Blue', 'Rouge']
         }
     }
 
-    onChange = (e) => {
-       // const formData = new FormData(form.current);
-        //store.setFormData(formData);
-    }
 
     joinGame = (id) => {
         axios.post('http://localhost:8000/api/players', {
@@ -71,7 +65,7 @@ class SearchGame extends Component {
                         <div className="title-page">Rechercher une partie</div>
     
                         {/********************** REASEARCH FORM **********************/}
-                        <form onChange={this.onChange} ref={this.form}>
+                        <form    ref={this.form}>
     
                             <label className="custom-label">Rerchercher par nom</label>
                             <input
@@ -90,14 +84,14 @@ class SearchGame extends Component {
                                 }
                             </select>
     
-                            <label className="custom-label">Rechercher par propriété</label>
+                            {/* <label className="custom-label">Rechercher par propriété</label>
                             <select className="custom-dropdown" name="propertySelect">
                                 {
                                     this.state.properties.map((e, i) => <option key={i} value={e}>
                                         {e}
                                     </option>)
                                 }
-                            </select>
+                            </select> */}
                         </form>
     
                         {/********************** RESULT TAB **********************/}
@@ -134,118 +128,3 @@ class SearchGame extends Component {
 }
 
 export default withRouter(SearchGame);
-
-
-
-//const SearchGame = observer(({ store }) => {
-
-    //const history = useHistory();
-/*
-    const properties = {
-        public: 'Publique',
-        private: 'Privée'
-    };
-    const maps = {
-        all: 'Toutes',
-        green: 'Vertes',
-        blue: 'Bleues',
-        red: 'Rouges',
-    };
-
-    const form = useRef(null);*/
-/*
-    const onChange = () => {
-        const formData = new FormData(form.current);
-        store.setFormData(formData);
-    }
-
-    const joinGame = (id) => {
-        axios.post('http://localhost:8000/api/players', {
-            round: 1,
-            life: 3,
-            game: '/api/games/' + id,
-            user: '/api/users/' + localStorage.getItem('userId')
-        }, {
-            headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-            }
-        }).then(res => {
-            if(res.status === 201) {
-                history.push('/saloon/' + id)
-            }
-        }).catch(err => {
-            console.log(err);
-        })
-    }*/
-
-
-
-  /*  return (
-        <>
-            <Menu />
-            <div className="searchGames">
-                <div className="main">
-                    <div className="title-page">Rechercher une partie</div>
-
-                    {/********************** REASEARCH FORM **********************///}
-                   /* <form onChange={onChange} ref={form}>
-
-                        <label className="custom-label">Rerchercher par nom</label>
-                        <input
-                            type="search"
-                            placeholder="Rechercher par nom"
-                            name="search"
-                            className="custom-input"
-                        />
-
-                        <label className="custom-label">Rechercher par carte</label>
-                        <select className="custom-dropdown" name="mapSelect">
-                            {
-                                Object.entries(maps).map((e, i) => <option key={i} value={e[0]}>
-                                    {e[1]}
-                                </option>)
-                            }
-                        </select>
-
-                        <label className="custom-label">Rechercher par propriété</label>
-                        <select className="custom-dropdown" name="propertySelect">
-                            {
-                                Object.entries(properties).map((e, i) => <option key={i} value={e[0]}>
-                                    {e[1]}
-                                </option>)
-                            }
-                        </select>
-                    </form>
-
-                    {/********************** RESULT TAB **********************///}
-                   /* <table className="custom-table">
-                        <thead>
-                            <tr>
-                                <th>Nom</th>
-                                <th>Carte</th>
-                                <th>Action</th>
-                                <th></th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {store.games.map((item) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <td>{item.name}</td>
-                                        <td>{maps[item.map]}</td>
-                                        <td>
-                                            <button className="custom-button" onClick={() => joinGame(item.id)}>Rejoindre</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-
-                </div>
-            </div>
-        </>
-    )
-}
-);*/
